76de5f5945925d490a116d7d2aa534d0be6f1916
CeciliaBdev <cecilia.bernard@live.fr>
2022-11-24 21:40:18 +0100

    update home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+import Accueil from './pages/Accueil'
+
 function App() {
-  return <div className="App font-light ">Test </div>
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Accueil />} />
+        <Route path="/createUser" />
+        <Route path="/listUsers" />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
---
+++ src/pages/Accueil.js
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom'
+import bg from '../assets/background.jpg'
+import logo from '../assets/logo-HRNet.svg'
+
+function Accueil() {
+  return (
+    <div className="bg-lime-400/20 h-screen w-screen">
+      {/* <img
+        src={bg}
+        alt="backgound"
+        className="opacity-10 h-screen w-screen object-cover relative"
+      /> */}
+      <div className="bg-white w-1/3 h-1/2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 py-5">
+        <div className="grid grid-rows-3  items-center h-full content-center  ">
+          <img src={logo} alt="logo hrnet" className="w-24 mx-auto" />
+          <h1 className="text-center text-lime-700 font-bold text-2xl">
+            HRnet
+          </h1>
+          <div className="grid grid-cols-2  h-12 ">
+            <div className=" h-full w-full  hover:border hover:bg-lime-400/50 flex justify-center items-center">
+              <Link to="/createUser">Create User</Link>
+            </div>
+            <div className=" h-full w-full  hover:border hover:bg-lime-500/50 flex justify-center items-center">
+              <Link to="/listUsers"> List</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Accueil
