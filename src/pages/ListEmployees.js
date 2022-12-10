ba5f2abda9d7ab9ef4094acd23bd201f1e86414b
CeciliaBdev <cecilia.bernard@live.fr>
2022-12-10 20:38:15 +0100

    commit before test useForm

--- src/pages/ListEmployees.js
+++ src/pages/ListEmployees.js
@@ -7,9 +7,9 @@ function ListEmployees() {
   return (
     <div className="bg-lime-400/20 h-screen w-screen ">
       <Link to="/" className="">
-        <img src={logo} alt="logo hrnet" className="w-24 mx-auto p-5" />
+        <img src={logo} alt="logo hrnet" className="w-24 mx-auto p-6" />
       </Link>
-      <div className="bg-white w-[100%] h-min absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center py-5">
+      <div className="bg-white w-[100%] h-min absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center py-4">
         <h1 className="font-bold text-2xl mx-5">Current employees</h1>
         <List />
       </div>
